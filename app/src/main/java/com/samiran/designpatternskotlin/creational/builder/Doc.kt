808bb28eaeec27a4144ba62a9892b0e9e59db8d5
SamiranDevo <samirankumar11@gmail.com>
2023-11-01 16:50:09 +0600

    Builder Design pattern documentation

---
+++ app/src/main/java/com/samiran/designpatternskotlin/creational/builder/Doc.kt
@@ -0,0 +1,32 @@
+package com.samiran.designpatternskotlin.creational.builder
+
+
+/**
+Created by Samiran Kumar on 01,November,2023
+ **/
+
+
+/**
+
+ What is Builder Design pattern?
+ ========================================================================
+ # Builder is a creational design pattern that lets you construct complex
+ objects step by step. The pattern allows you to produce different types
+ and representations of an object using the same construction code.
+
+ # Creational Design Pattern
+
+ # Builder design pattern is an alternative wey to construct complex objects
+ and should be used only when we want to build different types of immutable
+
+ objects using same object building process.
+
+
+ When Use
+ ========================================================================
+ Imagine a complex object that requires laborious, step-by-step initialization of
+ many fields and nested objects. Such initialization code is usually buried inside a
+ monstrous constructor with lots of parameters. Or even worse: scattered all over the
+ client code.
+
+ */
